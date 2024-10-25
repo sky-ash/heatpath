f79df0abbb2ef02b8064e92c01edff061a5ecfc2
sky-ash <sky-ash-dev@proton.me>
2024-10-25 23:58:07 +0200

    pages workflow

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/heatpath/',
+  build: {
+    outDir: 'dist',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-  },
-  base: '/heatpath/',
+  }
 })
