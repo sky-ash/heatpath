6f8db53c789fff108fa92b2f8d125b48020021b0
sky-ash <5kyl3r@proton.me>
2024-11-04 01:21:43 +0100

    Update Sources.jsx

--- src/pages/Sources.jsx
+++ src/pages/Sources.jsx
@@ -32,6 +32,14 @@ export default function Sources() {
                 ))}
             </List>
             <Navigation />
+                    <Link to="/settings">
+          <Fab color="primary"
+              sx={{ position: 'absolute', zIndex: 1,
+                    margin: '0 auto', top: -20,
+                    left: 0, right: 0, }}>
+            <SettingsIcon />
+          </Fab>
+        </Link>
         </>
     );
 }
